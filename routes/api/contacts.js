e64bbd81b48c5af7209b4a8772624e3ad2638e9b
Trunova Viktoria <vikit2000@ukr.net>
2023-04-10 00:17:31 +0300

    all tasks done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,30 @@
 const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
-const { validateBody } = require('../../middlewares');
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId} = require('../../middlewares');
+const {schemas} = require("../../models/contact");
 
 const router = express.Router()
 
 router.get('/', ctrl.getContactList)
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
 router.post("/",validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:contactId", ctrl.deleteContact);
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
 
-router.put("/:contactId",validateBody(schemas.updateSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.updateSchema),
+  ctrl.updateContact
+);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router
